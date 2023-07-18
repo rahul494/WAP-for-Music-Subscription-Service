8adc8b5b302029611b1c16fa9246496e9083cd9e
rahul494 <sharmarah494@gmail.com>
2023-07-17 22:17:44 -0400

    Added artist data via insert statements

---
+++ MySQL DML/artist_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Adele', 'Adkins', FALSE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Taylor', 'Swift', TRUE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Bruno', 'Mars', TRUE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Billie', 'Eilish', FALSE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Ed', 'Sheeran', TRUE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Justin', 'Bieber', FALSE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Beyonce', NULL, TRUE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Drake', NULL, FALSE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Rihanna', NULL, TRUE);
+INSERT INTO artists (first_name, last_name, on_tour) VALUES ('Kendrick', 'Lamar', FALSE);
--- common_queries.sql
+++ common_queries.sql
@@ -1,4 +1,4 @@
--- Display the frequency of subscriptions of all users
+-- 1. Display the frequency of subscriptions of all users
 
 -- left join is used because not everyone has a subscription
 select s.sub_name, count(*)
@@ -6,5 +6,3 @@ from users u
 left join user_subscriptions us on u.user_id = us.user_id
 left join subscriptions s on s.sub_id = us.sub_id
 group by s.sub_name
-
-
